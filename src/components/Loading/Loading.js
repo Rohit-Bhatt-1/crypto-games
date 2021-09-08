7658c2996b0be1862245c6aa5bef851df42f15aa
YOG3SHWAR <yogeshwar.chaturvedi@publicissapient.com>
2021-09-08 17:13:03 +0530

    loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,15 @@
-import React from "react";
+import pleasewait from "../../assests/pleasewait.png";
+import { motion } from "framer-motion";
+
 function Loading() {
   return (
-    <div className="loading">
-      <img src="../../../public/assests/pleasewait.png" alt="Loading" />
-    </div>
+    <motion.div
+      className="loading"
+      initial={{ x: "100%" }}
+      animate={{ x: "calc(100vw - 50%)" }}
+    >
+      <img src={pleasewait} alt="Loading" />
+    </motion.div>
   );
 }
 
